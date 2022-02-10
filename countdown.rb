183ccae6936815a4b623473a5a4fced97ae9c6e4
Read Langworthy <rlangworthy@gmail.com>
2022-02-10 10:26:11 -0800

    commit tree 3647b910ac9e4dc48cf9b86666f2ceea7adfab8a

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,27 @@
 #write your code here
 
-def countdown
+def countdown int
+    while int > 0
+        puts "#{int} SECOND(S)!"
+        int -= 1
+    end
+    "HAPPY NEW YEAR!"
+
 end
+
+
+
+# puts countdown 10
+
+
+def countdown_with_sleep int
+    while int > 0
+        puts "#{int} SECONDS(S)!"
+        sleep(1)
+        int -= 1
+
+    end
+    "HAPPY NEW YEAR!"
+end
+
+puts countdown_with_sleep 10
